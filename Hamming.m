5eae7a63f3acaef39405ed45790a47887027445c
rafatespindola <rafatespindola@gmail.com>
2019-10-03 15:09:33 -0300

    Codigo aula 03/10

--- Hamming.m
+++ Hamming.m
@@ -1,37 +1,32 @@
-%Hamming
-%Low Pass
-clear all
+%% Filtro passa faixa
 close all
 clc
 
-fp = 10e3;
-fs = 15e3;
-fa = 50e3; %Frequencia de amostragem
+fa = 50e3; %freq amostragem
+As = 50;   %atenuaçao stop
+Ap = 0.2;  %atenuaçao passagem
 
-wa = 2*pi*fa;
-ws = 2*pi*fs;
-wp = 2*pi*fp;
-wc = sqrt(ws*wp); 
+ws1 = 0.1*pi; wp1 = 0.2*pi;
+wp2 = 0.3*pi; ws2 = 0.4*pi;
 
-teta_c = wc/(wa/2);
+teta_c1 = sqrt(wp1*ws1);
+teta_c2 = sqrt(wp2*ws2);
 
 ordem = 100;
 M = ordem/2;
 n = -M:M;
-w_n = 0.54 + 0.46*cos((2*pi*n)/(2*M + 1));
-w_han = 0.5 + 0.5*cos((2*pi*n)/(2*M + 1));
-clp = sin(teta_c.*pi.*n)./(pi*n);
-clp(M+1) = teta_c;
-hamming = w_n.*clp;
-han = w_han.*clp;
-fvtool(han, 1, hamming, 1, clp,1);
-legend('Hanning', 'Hamming', 'Retangular')
+n_medido = ordem;
 
+w_hamm = 0.54 + 0.46*cos((2*pi.*n)/(2*M+1));
+w_han = 0.5 + 0.5*cos((2*pi.*n)/(2*M+1));
 
+cbp = (sin(teta_c2*n)-sin(teta_c1*n))./(pi*n) % teta_c -> na wiki eh Wc
+cbp(M+1) = (teta_c2 - teta_c1)./pi;
 
+hamming = w_hamm.*cbp;
+hann = w_han.*cbp;
 
-
-
-
-
-
+fvtool(hann,1,hamming,1,cbp,1)
+legend('hann','hamming','retangular')
+filtro_coef_fourier.m
+Exibindo filtro_coef_fourier.m.
